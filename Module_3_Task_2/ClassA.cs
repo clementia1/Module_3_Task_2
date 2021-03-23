6a4d54e8f59ae3b5ec08481c03fa5213b621775b
Михаил Гринин <Grinin@kh.dcz.gov.ua>
2021-03-23 12:50:54 +0200

    Update ClassA.cs

--- Module_3_Task_2/ClassA.cs
+++ Module_3_Task_2/ClassA.cs
@@ -6,9 +6,14 @@ namespace Module_3_Task_2
 {
     public class ClassA
     {
+        public ClassA()
+        {
+            PowHandler = Pow;
+        }
+
         public Action<bool> ShowHandler { get; set; }
 
-        public Func<int, int, int> PowHandler { get; set; } = new ClassA().Pow;
+        public Func<int, int, int> PowHandler { get; set; }
 
         public int Pow(int a, int b)
         {
